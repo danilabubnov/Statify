72903842ced1dff4f2ab139b5fd7e6d0eab87ee8
Danila <danila.bubnov@softwea.com>
2025-03-19 03:56:09 +0300

    setup ObjectMapper

--- statify-utils/src/main/kotlin/config/JacksonConfiguration.kt
+++ statify-utils/src/main/kotlin/config/JacksonConfiguration.kt
@@ -1,4 +1,25 @@
 package config
 
-class JacksonConfiguration {
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class JacksonConfiguration {
+
+    @Bean
+    open fun objectMapper(): ObjectMapper = ObjectMapper()
+        .registerModule(KotlinModule.Builder().build())
+        .registerModule(
+            Hibernate5JakartaModule().configure(
+                Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, false
+            )
+        )
+        .registerModule(JavaTimeModule())
+        .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+
 }
