983353c137308e349eeceda7aa2c0a41b6e62690
Danila <danila.bubnov@softwea.com>
2025-03-19 03:53:49 +0300

    configure Swagger

---
+++ statify-core/src/main/kotlin/configuration/OpenApiConfig.kt
@@ -0,0 +1,25 @@
+package org.danila.configuration
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+class OpenApiConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI? {
+        return OpenAPI()
+            .addSecurityItem(SecurityRequirement().addList("bearerAuth"))
+    }
+
+}
