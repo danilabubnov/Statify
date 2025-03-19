f5ad070779065ea1f7452cce728fff02207157aa
Danila <danila.bubnov@softwea.com>
2025-03-19 03:55:30 +0300

    integrate dotenv for environment variable loading and configure entity scanning

--- statify-core/src/main/kotlin/StatifyCoreApplication.kt
+++ statify-core/src/main/kotlin/StatifyCoreApplication.kt
@@ -1,11 +1,15 @@
 package org.danila
 
+import io.github.cdimascio.dotenv.DotenvBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
+@EntityScan(basePackages = ["org.danila.model"])
 @SpringBootApplication
 class StatifyCoreApplication
 
 fun main(args: Array<String>) {
+    DotenvBuilder().systemProperties().load()
     runApplication<StatifyCoreApplication>(*args)
 }
