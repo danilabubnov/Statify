aa4843d40cdf7df2a51ecbcfc4fbad44a4dcc830
Danila <danila.bubnov@softwea.com>
2025-03-28 00:19:35 +0300

    Initialize statify-synchronizer module: add Dockerfile, Gradle wrapper, and main application file

--- statify-synchronizer/src/main/kotlin/StatifySynchronizerApplication.kt
+++ statify-synchronizer/src/main/kotlin/StatifySynchronizerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class StatifyCoreApplication
+class StatifySynchronizerApplication
 
 fun main(args: Array<String>) {
-    runApplication<StatifyCoreApplication>(*args)
+    runApplication<StatifySynchronizerApplication>(*args)
 }
